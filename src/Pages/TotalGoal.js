5da8682561f34aa2927288ffe2bfdf130278cabf
Kijoon <igreen0485@gmail.com>
2021-11-23 17:21:37 +0900

    add new ranger

--- src/Pages/TotalGoal.js
+++ src/Pages/TotalGoal.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import DraggableGraph from "../Components/Graphs/DraggableGraph";
 import SectionTitle from "../Components/SectionTitle";
 import { connect } from "react-redux";
 import { convertNumToTime } from "../Components/Functions/convertNumToTime";
+import { Range, getTrackBackground } from 'react-range';
 
 import "../static/customStyle.css";
 import "./Goal.css";
@@ -14,6 +15,7 @@ const mapStateToProps = (state) => ({
 
 const TotalGoal = (props) => {
   const history = useHistory();
+  const [values, setValues] = useState([50])
 
   const handleRoute = (path) => {
     history.push(path);
@@ -58,6 +60,79 @@ const TotalGoal = (props) => {
               <DraggableGraph />
             </div>
           </div>
+          <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          margin: "2em"
+        }}
+      >
+        <Range
+          values={values}
+          step={0.1}
+          min={0}
+          max={100}
+          onChange={(values) => setValues(values)}
+          renderTrack={({ props, children }) => (
+            <div
+              onMouseDown={props.onMouseDown}
+              onTouchStart={props.onTouchStart}
+              style={{
+                ...props.style,
+                height: "36px",
+                display: "flex",
+                width: "100%"
+              }}
+            >
+              <div
+                ref={props.ref}
+                style={{
+                  height: "5px",
+                  width: "100%",
+                  borderRadius: "4px",
+                  background: getTrackBackground({
+                    values: values,
+                    colors: ["#548BF4", "#ccc"],
+                    min: 0,
+                    max: 100
+                  }),
+                  alignSelf: "center"
+                }}
+              >
+                {children}
+              </div>
+            </div>
+          )}
+          renderThumb={({ props, isDragged }) => (
+            <div
+              {...props}
+              style={{
+                ...props.style,
+                height: "42px",
+                width: "42px",
+                borderRadius: "4px",
+                backgroundColor: "#FFF",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                boxShadow: "0px 2px 6px #AAA"
+              }}
+            >
+              <div
+                style={{
+                  height: "16px",
+                  width: "5px",
+                  backgroundColor: isDragged ? "#548BF4" : "#CCC"
+                }}
+              />
+            </div>
+          )}
+        />
+        <output style={{ marginTop: "30px" }} id="output">
+          {values[0].toFixed(1)}
+        </output>
+      </div>
         </div>
       </div>
     </div>
